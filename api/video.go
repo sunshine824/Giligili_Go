33d6b53ac46cca35c7294587a2a57b0805ceb74b
阳晨 <790168710@qq.com>
2020-01-19 23:12:07 +0800

    视频投稿api实现

---
+++ api/video.go
@@ -0,0 +1,14 @@
+package api
+
+import (
+	"giligili/serializer"
+	"github.com/gin-gonic/gin"
+)
+
+// CreateVideo 视频投稿
+func CreateVideo(c *gin.Context) {
+	c.JSON(200, serializer.Response{
+		Code: 0,
+		Msg:  "成功",
+	})
+}
